20179f69f73234ced702ccd5eea3b7b15e92cba0
parkhomIsCreative <alexandr.parkhomenko@gmail.com>
2021-12-01 22:14:22 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Changes to be committed:
    	deleted:    client/src/app/model/Image.ts
    	deleted:    client/src/app/models/Comment.ts
    	deleted:    client/src/app/models/Post.ts
    	deleted:    client/src/app/models/User.ts
    
     Changes not staged for commit:
    	modified:   client/src/app/layout/index/index.component.html
    	modified:   client/src/app/layout/index/index.component.ts
    	modified:   client/src/app/layout/navigation/navigation.component.ts
    	modified:   client/src/app/model/Post.ts
    	modified:   client/src/app/service/comment.service.ts
    	modified:   client/src/app/service/post.service.ts
    	modified:   client/src/app/user/add-post/add-post.component.ts
    	modified:   client/src/app/user/edit-user/edit-user.component.ts
    	modified:   client/src/app/user/profile/profile.component.ts
    	modified:   client/src/app/user/user-posts/user-posts.component.ts
    
     Untracked files:
    	.gitignore

--- client/src/app/model/Image.ts
+++
@@ -1,6 +0,0 @@
-export interface Image{
-  id : number;
-  imageURL : string;
-  username? : string;
-  postid? : number;
-}
--- client/src/app/models/User.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  id: number;
-  email: string;
-  username: string;
-  firstname: string;
-  lastname: string;
-  bio?: string;
-}
